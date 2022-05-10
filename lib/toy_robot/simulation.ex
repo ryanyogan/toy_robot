5e443164ca83a9336bfcf5881597b466a89234f5
Ryan Yogan <ryanyogan@gmail.com>
2022-05-10 15:18:57 -0500

    Simulation may now move and report on a robot position // added ExDocs

--- lib/toy_robot/simulation.ex
+++ lib/toy_robot/simulation.ex
@@ -34,6 +34,114 @@ defmodule ToyRobot.Simulation do
     maybe_place(table, placement, Table.valid_position?(table, placement))
   end
 
+  @doc """
+  Moves the robot forward one space in the direction that it is
+  facing, unless that position is past the boundaries of the table.
+
+  ## Examples
+
+  ### A valid movement
+
+      iex> alias ToyRobot.{Robot, Table, Simulation}
+      [ToyRobot.Robot, ToyRobot.Table, ToyRobot.Simulation]
+      iex> table = %Table{north_boundary: 4, east_boundary: 4}
+      %Table{north_boundary: 4, east_boundary: 4}
+      iex> simulation = %Simulation{
+      ...>  table: table,
+      ...>  robot: %Robot{north: 0, east: 0, facing: :north}
+      ...> }
+      iex> simulation |> Simulation.move()
+      {:ok, %Simulation{
+        table: table,
+        robot: %Robot{north: 1, east: 0, facing: :north}
+      }}
+
+  ### An invalid movement
+
+      iex> alias ToyRobot.{Robot, Table, Simulation}
+      [ToyRobot.Robot, ToyRobot.Table, ToyRobot.Simulation]
+      iex> table = %Table{north_boundary: 4, east_boundary: 4}
+      %Table{north_boundary: 4, east_boundary: 4}
+      iex> simulation = %Simulation{
+      ...>  table: table,
+      ...>  robot: %Robot{north: 4, east: 0, facing: :north}
+      ...> }
+      iex> simulation |> Simulation.move()
+      {:error, :at_table_boundary}
+  """
+  def move(%{robot: robot, table: table} = simulation) do
+    with moved_robot <- Robot.move(robot),
+         true <- Table.valid_position?(table, moved_robot) do
+      {:ok, %{simulation | robot: moved_robot}}
+    else
+      false -> {:error, :at_table_boundary}
+    end
+  end
+
+  @doc """
+  Turns the robot left.
+
+  ## Examples
+
+      iex> alias ToyRobot.{Robot, Table, Simulation}
+      [ToyRobot.Robot, ToyRobot.Table, ToyRobot.Simulation]
+      iex> table = %Table{north_boundary: 4, east_boundary: 4}
+      %Table{north_boundary: 4, east_boundary: 4}
+      iex> simulation = %Simulation{
+      ...>  table: table,
+      ...>  robot: %Robot{north: 0, east: 0, facing: :north}
+      ...> }
+      iex> simulation |> Simulation.turn_left()
+      {:ok, %Simulation{
+        table: table,
+        robot: %Robot{north: 0, east: 0, facing: :west}
+      }}
+  """
+  def turn_left(%Simulation{robot: robot} = simulation) do
+    {:ok, %{simulation | robot: robot |> Robot.turn_left()}}
+  end
+
+  @doc """
+  Turns the robot right.
+
+  ## Examples
+
+      iex> alias ToyRobot.{Robot, Table, Simulation}
+      [ToyRobot.Robot, ToyRobot.Table, ToyRobot.Simulation]
+      iex> table = %Table{north_boundary: 4, east_boundary: 4}
+      %Table{north_boundary: 4, east_boundary: 4}
+      iex> simulation = %Simulation{
+      ...>  table: table,
+      ...>  robot: %Robot{north: 0, east: 0, facing: :north}
+      ...> }
+      iex> simulation |> Simulation.turn_right()
+      {:ok, %Simulation{
+        table: table,
+        robot: %Robot{north: 0, east: 0, facing: :east}
+      }}
+  """
+  def turn_right(%Simulation{robot: robot} = simulation) do
+    {:ok, %{simulation | robot: robot |> Robot.turn_right()}}
+  end
+
+  @doc """
+  Returns the robot's current position.
+
+  ## Examples
+
+      iex> alias ToyRobot.{Robot, Table, Simulation}
+      [ToyRobot.Robot, ToyRobot.Table, ToyRobot.Simulation]
+      iex> table = %Table{north_boundary: 4, east_boundary: 4}
+      %Table{north_boundary: 4, east_boundary: 4}
+      iex> simulation = %Simulation{
+      ...>  table: table,
+      ...>  robot: %Robot{north: 0, east: 0, facing: :north}
+      ...> }
+      iex> simulation |> Simulation.report()
+      %Robot{north: 0, east: 0, facing: :north}
+  """
+  def report(%Simulation{robot: robot}), do: robot
+
   defp maybe_place(table, placement, true) do
     {
       :ok,
